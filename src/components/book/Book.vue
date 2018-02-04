b53bd2aa9ab6b7471b272bec765c96a186a9d7e6
LEE66612138 <1131197832@qq.com>
2018-02-04 08:59:18 +0800

    book

--- src/components/book/Book.vue
+++ src/components/book/Book.vue
@@ -5,6 +5,12 @@
     <!--</common-header>-->
     <!--<common-footer bgColor="rgb(121, 85, 72)"></common-footer>-->
   <!--</div>-->
+
+  <swipe class="my-swipe">
+    <swipe-item class="slide1"></swipe-item>
+    <swipe-item class="slide2"></swipe-item>
+    <swipe-item class="slide3"></swipe-item>
+  </swipe>
 </template>
 
 <script>
@@ -16,8 +22,38 @@
 //      CommonFooter
 //    }
 //  }
+import "vue-swipe/dist/vue-swipe.css";
+import { Swipe, SwipeItem } from "vue-swipe";
+export default {
+  components: {
+    swipe: Swipe,
+    "swipe-item": SwipeItem
+  }
+};
+
 </script>
 
 <style scoped>
 
+  .my-swipe {
+    height: 200px;
+    color: #fff;
+    font-size: 30px;
+    text-align: center;
+  }
+
+  .slide1 {
+    background-image: url('https://img3.doubanio.com/lpic/s27102925.jpg');
+    color: #fff;
+  }
+
+  .slide2 {
+    background-image: url('https://img3.doubanio.com/lpic/s6989253.jpg');
+    color: #000;
+  }
+
+  .slide3 {
+    background-image: url('https://img3.doubanio.com/lpic/s24468373.jpg');
+    color: #fff;
+  }
 </style>
--- src/components/movie/Movie.vue
+++ src/components/movie/Movie.vue
@@ -29,7 +29,7 @@
 </template>
 
 <script>
-//import CommonHeader from '@/components/common/Header'
+// CommonHeader from '@/components/common/Header'
 //import CommonFooter from '@/components/common/Footer'
 //export default {
 //  components: {
